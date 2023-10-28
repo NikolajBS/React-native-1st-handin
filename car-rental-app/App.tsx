9f1632b6d6e5deeb714ae453a6a434c85f768316
NikolajBS <nstau21@student.sdu.dk>
2023-10-28 09:09:18 +0200

    Home page

--- car-rental-app/App.tsx
+++ car-rental-app/App.tsx
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/Home';
+import { FontAwesome } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator 
+          screenOptions={{
+            tabBarStyle: {
+              backgroundColor: '#c4c4c4'
+            }
+          }}>
+          <Tab.Screen name="Home" component={HomeScreen} options={{
+            tabBarStyle: { backgroundColor: '#a4a4a4'},
+            tabBarLabel: 'Home', 
+            tabBarIcon: () => (
+            <FontAwesome name="home" color={'#000000'} size={40} />
+          ),}}/>
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
