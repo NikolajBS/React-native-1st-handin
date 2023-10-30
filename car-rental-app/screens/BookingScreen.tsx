18e5e249e9096685160741e3cd5de520bb3bec80
weasy1as <habdu21@student.sdu.dk>
2023-10-30 18:32:37 +0100

    dd

--- car-rental-app/screens/BookingScreen.tsx
+++ car-rental-app/screens/BookingScreen.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { images } from '../images';
+
+function CarBox({ name, price, duration, date, imageIndex }) {
+  const carImage = images[imageIndex];
+
+  return (
+    <View style={styles.carComponent}>
+      <Image source={carImage} style={styles.carImage} />
+      <Text>Name: {name}</Text>
+      <Text>Price: {price}</Text>
+      <Text>Duration: {duration}</Text>
+      <Text>Date: {date}</Text>
+    </View>
+  );
+}
 
 function BookingScreen({ route }) {
   return (
     <View style={styles.container}>
-      <Text>Booking Screen Content</Text>
-      {/* Add your additional content here */}
+      <Text style={styles.txt}>Your Bookings</Text>
+      <CarBox name="Toyota" price="$50" duration="2 days" date="Nov 10, 2023" imageIndex={0} />
+      <CarBox name="Mercedes" price="$60" duration="3 days" date="Nov 15, 2023" imageIndex={1} />
     </View>
   );
 }
@@ -16,7 +32,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  // You can add more styles here for your component
+  carComponent: {
+    flex: 1,
+    backgroundColor: '#f0ae5d',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  carImage: {
+    width: 100,
+    height: 100,
+  },
+  txt: {
+    fontSize: 40,
+  },
 });
 
 export default BookingScreen;
